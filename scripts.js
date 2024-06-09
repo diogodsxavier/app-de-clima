e8450245eda1bda73939167533b8813dd2ffcd02
Diogo Xavier <diogodsxavier@gmail.com>
2024-06-09 20:18:44 -0300

    corrigindo a temperatura

--- scripts.js
+++ scripts.js
@@ -3,11 +3,12 @@ function colocarDadosNaTela(dados) {
     document.querySelector('.cidade').innerHTML = 'Tempo em ' + dados.name;
     document.querySelector('.temp').innerHTML = `${Math.floor(dados.main.temp)}°C`;
     document.querySelector('.texto-previsao').innerHTML = dados.weather[0].description;
+    document.querySelector('.max-temp').innerHTML = `Máx: ${Math.floor(dados.main.temp_max)}°`;
+    document.querySelector('.min-temp').innerHTML = `Min: ${Math.floor(dados.main.temp_min)}°`;
     document.querySelector('.umidade').innerHTML = `Umidade ${dados.main.humidity}%`
     document.querySelector('.img-previsao').src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
 
-    document.querySelector('.max-temp').innerHTML = `Máx: ${Math.floor(dados.main.temp_max)}`;
-    document.querySelector('.min-temp').innerHTML = `Min: ${Math.floor(dados.main.temp_min)}`;
+
 }
 
 async function buscarCidade(cidade) {
